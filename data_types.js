744ff4a834f5308e8c26795376a1204235ab7f6b
varunkumar9520 <vkg952012@gmail.com>
2024-05-27 06:07:41 +0000

    datatypes

---
+++ data_types.js
@@ -0,0 +1,26 @@
+"use srtrict"; // it treates the js code as newer version
+
+// alert(33+3);  we are using nodejs not using browser
+
+console.log("varun")
+// console.log(2+3
+
+
+
+// ) // we can print values as this but the code readability should be high
+
+let name="varun"
+let age=18
+let city ="modinagar"
+
+/* 
+  data types :-
+  1.string->" "
+  2.interger(number)->> 2 power 53
+  3.boolean-> true/false
+  4.bigInt ->it can hold values greter than number
+  5.null-> it means nothing it is a standalone value
+  6.undefined->when we donot give any value to the function
+*/
+
+// type of-> it tells the what type of value it holds
---
+++ variable_declartion.js
@@ -0,0 +1,11 @@
+const accounId = 14254  // this value is constant we cannot make any changes in it
+
+let accountEmail="hello@gmail.com" // it is way of declaration of variable in js
+
+var accountCity="modinagar" // we can declare as by "var" keyword but generally we can't use it because it has problem with local scope and functional scope
+
+// accounId=14565  we cannot make any changes in the const value after the declaration
+
+console.log("accounId is: ",accounId);
+console.log(accountEmail)
+console.log(accountCity);
