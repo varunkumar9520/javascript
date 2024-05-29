de016bb8d4efcc24996fc234f45899663ec2c2be
varunkumar9520 <vkg952012@gmail.com>
2024-05-29 04:33:53 +0000

    conversion

--- type_conversion.js
+++ type_conversion.js
@@ -1,2 +1,44 @@
-let score=12
+let score="12"
+// console.log(typeof score);
+
+let value=Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+let string="abc123"
+// console.log(typeof string);
+let value1 = Number(string)
+// console.log(typeof value1)
+// console.log(value1)
+
+let variable=undefined
+// console.log(typeof variable)
+
+let bool =true;
+// console.log(typeof bool);
+
+
+// important points
+
+// "33"=> 33
+// "33abc"=>NaN(not a number)
+// true =>1
+// false=>0
+
+let isLoggedIn = "varun"
+let boolLoggedIn = Boolean(isLoggedIn);
+// console.log(boolLoggedIn)
+
+// 1=>true 0=>false
+//""=>false; "hitesh"=>true
+
+let sum = 33
+// console.log("before conversion:");
+// console.log(typeof sum);
+let strnum = String(sum);
+// console.log("after conversion:");
+// console.log(typeof strnum);
+// console.log(strnum)
+
+
 
