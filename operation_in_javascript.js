afcb69e1b44e2f20aa4ea98130101f9f61f5c8e6
varunkumar9520 <vkg952012@gmail.com>
2024-06-02 09:38:04 +0000

    operation

---
+++ operation_in_javascript.js
@@ -0,0 +1,22 @@
+let value = 10;
+let negvalue = -value;
+
+// console.log(value);
+// console.log(negvalue);
+
+//(2+2) addition
+// (2*2) multiplication
+// (2-2) subtraction
+// (2/3) division
+// (2%3) modulus
+
+let str = "hello"
+let str1 =" varun"
+let str2 = str+str1; // concatenation of two striing 
+// console.log(str2);
+
+console.log("1"+2); // there's some kind of rule in javascript is that if the first value is string then every upcoming values will be converted into string-> 12
+console.log(1+"2"); 
+console.log("1"+2+2);
+console.log(1+2+"2"); // in this firstly the operations is perfomed after that it is convert
+
